3d5b405a14c9fccb71cc88b1325e0a13719b8423
angela balaguer <balaguer.valles@gmail.com>
2019-12-01 17:16:13 -0500

    FusedLocation y FusedLocationClient

--- app/src/main/java/com/example/taller3/MainActivity.java
+++ app/src/main/java/com/example/taller3/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.taller3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,7 +35,46 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment)
                 getSupportFragmentManager().findFragmentById(R.id.mapa);
         mapFragment.getMapAsync(this);
-    }
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+        {
+            Toast.makeText(this, "GPS available", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Toast.makeText(this, "GPS not available", Toast.LENGTH_LONG).show();
+        }
+        LocationListener locationListener = new LocationListener()
+        {
+            public void onLocationChanged(Location location)
+            {
+                Toast.makeText(getApplicationContext(), "Se cambio de posicion", Toast.LENGTH_SHORT).show();
+                Double latitude=location.getLatitude();
+                Double longitude=location.getLatitude();
+                Toast.makeText(getApplicationContext(), "latitud: "+ latitude.toString()+ " longitud: "+ longitude.toString(), Toast.LENGTH_SHORT).show();
+                if(checkseguir==1)
+                    if (mapa.getMyLocation() != null)
+                        mapa.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                new LatLng(mapa.getMyLocation().getLatitude(),
+                                        mapa.getMyLocation().getLongitude()), 15));
+            }
+            public void onStatusChanged(String provider, int status, Bundle extras)
+            {
+            }
+            public void onProviderEnabled(String provider)
+            {
+            }
+            public void onProviderDisabled(String provider)
+            {
+            }
+        };
+        //pregunta si dcepta el permiso o no
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+ }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mapa = googleMap;
@@ -34,7 +82,18 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapa.addMarker(new MarkerOptions().position(ubicacion).title("Marcador Tacna"));
         mapa.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
         mapa.setOnMapClickListener(this);
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            mapa.setMyLocationEnabled(true);
+            mapa.getUiSettings().setZoomControlsEnabled(false);
+            mapa.getUiSettings().setCompassEnabled(true);
+        } else {
+            Button btnMiPos=(Button) findViewById(R.id.btnmiubi);
+            btnMiPos.setEnabled(false);
+        }
     }
+
     public void moveCamera(View view) {
         mapa.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
     }
@@ -48,5 +107,36 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         mapa.addMarker(new MarkerOptions().position(puntoPulsado)
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+    }
+
+    public void ubicacion(View view) {
+        if (mapa.getMyLocation() != null)
+            mapa.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(mapa.getMyLocation().getLatitude(),
+                            mapa.getMyLocation().getLongitude()), 15));
+
+    }
+
+    int checkseguir=0;
+    public void Seguir(View view) {
+        Button btnseguir=findViewById(R.id.btnseguir);
+        if (checkseguir==0) {
+            checkseguir = 1;
+            btnseguir.setText("OFF seguir");
+            Toast.makeText(this, "Se Activo el seguimiento", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            checkseguir = 0;
+            btnseguir.setText("ON seguir");
+            Toast.makeText(this, "Se desactivo el seguimiento", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void mifusedlocation(View view) {
+        startActivity(new Intent(this, MiFusedLocation.class));
+    }
+
+    public void migoogleapiclient(View view) {
+        startActivity(new Intent(this, MiFusedLocationClient.class));
     }
 }
